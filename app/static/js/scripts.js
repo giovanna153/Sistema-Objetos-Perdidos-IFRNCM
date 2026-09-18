d01cb6ecbf46c1109f69bd6d58349f37c6c9d029
Giovanna Souza <gioosouzaliveira@gmail.com>
2026-09-18 17:17:26 -0300

    adicionando as pastas

---
+++ app/static/js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
+* Copyright 2013-2023 Start Bootstrap
+* Licenciado sob a licença MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+*/
+// Este arquivo está vazio intencionalmente
+// Use este arquivo para adicionar JavaScript ao seu projeto
